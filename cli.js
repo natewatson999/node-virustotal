6408ba70a368a047a77e9a570bbb7c79f170ac2b
Nate Watson <watsonn2013@my.fit.edu>
2016-06-09 12:27:01 -0400

    cli work
    still need to figure out how to add queryBuilder to the commands

--- cli.js
+++ cli.js
@@ -8,6 +8,7 @@ var sendPrompt = function(){
   rl.setPrompt(">");
   rl.prompt();
 };
+var mode = "public";
 var defaultKeystore = {
   public: ["e2513a75f92a4169e8a47b4ab1df757f83ae45008b4a8a49903450c8402add4d"],
   honey: [],
@@ -24,6 +25,40 @@ rl.on("line", function(input){
     case "printKeyring":
     case "saveKeyring":
     case "loadKeyring":
+    case "printMode":
+    case "setMode":
+    case "setKey":
+    case "addKey":
+    case "deleteKey":
+    case "exit":
+    case "getDelay":
+    case "setDelay":
+    case "IPv4Report":
+    case "DomainReport":
+    case "submitURL":
+    case "getUrlReport":
+    case "publishUrlComment":
+    case "publishFileComment":
+    case "sendFile":
+    case "rescanFile":
+    case "getFileReport":
+    case "getUrlComments":
+    case "getFileComments":
+    case "getFile":
+    case "fileRescan":
+    case "getFileBehavior":
+    case "getFileNetworkActivity":
+    case "getClusters":
+    case "getFileFeed":
+    case "getUrlFeed":
+    case "getNextFalsePositive":
+    case "search":
+    case "exportRuleset":
+    case "deleteNotifications":
     
+    default:
+      console.log("feature not yet implemented");
+      sendPrompt();
+      return;
   }
 });
