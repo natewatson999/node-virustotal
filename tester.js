8c6244ed85f7c616c263a849d29c78e8cd3a5170
Nate Watson <watsonn2013@my.fit.edu>
2017-01-15 21:08:03 -0500

    depenency patch
    I was informed of a dependency vulnerability. This is a patch.

--- tester.js
+++ tester.js
@@ -15,17 +15,17 @@ con.getDomainReport("wikionemore.com",function(data){
   console.dir(data);
 }, function(err){
   console.error(err);
-});
+});/*
 con.submitUrlForScanning("http://wikionemore.com",function(data){
   console.dir(data);
 }, function(err){
   console.error(err);
-});
+});*/
 con.retrieveUrlAnalysis("http://wikionemore.com",function(data){
   console.dir(data);
 }, function(err){
   console.error(err);
-});
+});/*
 con.publishUrlComment("http://wikionemore.com", "Ignore this comment. I'm just testing an API.", function(data){
   console.dir(data);
 }, function(err){
@@ -50,24 +50,24 @@ con.publishFileComment("de053e0e115fc94a81eb3dc074b02c68efaa60ff4251f386e299d881
   console.dir(data);
 }, function(err){
   console.error(err);
-});
+});*/
 con.UrlEvaluation("http://wikionemore.com",function(data){
   console.dir(data);
 }, function(err){
   console.error(err);
-});
+});/*
 con.FileEvaluation("obvious_virus.svg", "text/svg", fs.readFileSync("./obvious_virus.svg"), function(data){
   console.log(data);
 }, function(mistake){
   console.log(mistake);
-});
-
+});*/
+/*
 var con = vt.makePrivateConnection();
 con.setKey("e2513a75f92a4169e8a47b4ab1df757f83ae45008b4a8a49903450c8402add4d");
 con.getIP4Report("90.156.201.27",function(data){
   console.dir(data);
 }, function(err){
   console.error(err);
-});
+});*/
 
 /*Sidenote: That's a real phishing site. It was shut down, but I still advise against going to it.*/
