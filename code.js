1bfd89edcc3818198d4687e5e4fffbbcd00a6d0f
Ivan Mikhailov <erinithma@gmail.com>
2017-12-04 12:38:54 +0300

    fix

--- code.js
+++ code.js
@@ -299,5 +299,7 @@ features.makeIapiConnection = intelAPI.makeIapiConnection;
 features.queryBuilder = QB;
 features.makePrivateConnection = privateAPI.makePrivateAPI;
 features.formatConverter = formatConverter;
-module.exports = features;
+module.exports = function(){
+	return features;
+};
 /*Still need to add a feature to make Yara files, and upload them.*/
